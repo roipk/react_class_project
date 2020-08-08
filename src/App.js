086157b887154685f2b37a1feb2884bb767cf7b0
Roi Madmon <roipk123@gmail.com>
2020-08-09 02:04:06 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Container>
+              <Row>
+                  <Col className="Card">
+                      <div> קבוצה 1 </div>
+                      <div> חברי הקבוצה </div>
+                      <div> אמג'ד בשיתי </div>
+                      <div> נורהאן עבדל רחמן </div>
+                      <div> שם : מוסא חסונא </div>
+                      <div> שם : רועי סולימני </div>
+                      <div> שם : תהילה כהן </div>
+                      <div> שם : לואיי ג'אבר </div>
+                      <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                      <div><a href="https://github.com/bashbash96/movies-list" target="_blank">מעבר לגיט הקבוצה</a></div>
+
+
+                  </Col>
+                  <Col className="Card">
+                      <div> קבוצה 2 </div>
+                      <div> חברי הקבוצה </div>
+                      <div> הדר דבאח </div>
+                      <div> יצחק שוורץ </div>
+                      <div> חן פרנסה </div>
+                      <div> רחלי וורכנזון </div>
+                      <div> מרים (שבתאי) הרו </div>
+                      <div> אודיה טהרני </div>
+                      <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                      <div><a href="https://github.com/hadardabah/react" target="_blank">מעבר לגיט הקבוצה</a></div>
+
+                  </Col>
+                  <Col className="Card">
+                      <div> קבוצה 3 </div>
+                      <div> חברי הקבוצה </div>
+                      <div> נועם לסרי </div>
+                      <div> אופיר עוזרי </div>
+                      <div> אמיר בסיג </div>
+                      <div> תומר מעלומי </div>
+                      <div> שמואל מרקובסקי </div>
+                      <div> סמואל נקש </div>
+                      <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                      <div><a href="https://github.com/noamlasry/React-Final-Project.git" target="_blank">מעבר לגיט הקבוצה</a></div>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col>
+                      <div className="Card">
+                          <div> קבוצה 4 </div>
+                          <div> חברי הקבוצה </div>
+                          <div> אנס אבו רמלה  </div>
+                          <div> פבל פטרנקו </div>
+                          <div> מחמוד קוואסמי </div>
+                          <div> אחמד שרבאתי </div>
+                          <div> דוחא כסואני </div>
+                          <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                          <div><a href="https://github.com/anaSameer/ReactFinalProject" target="_blank">מעבר לגיט הקבוצה</a></div>
+                      </div>
+                  </Col>
+                  <Col>
+                      <div className="Card">
+                          <div> קבוצה 5 </div>
+                          <div> חברי הקבוצה </div>
+                          <div> לידור זקן </div>
+                          <div> אורי תור </div>
+                          <div> אלעד אשר </div>
+                          <div> חיים פיסקרוב </div>
+                          <div> מאור דסה </div>
+                          <div> אור כהן </div>
+                          <div> ירין דנון </div>
+                          <div><a href="https://vgareus-1f6f2.web.app/"> מעבר לפרוייקט הקבוצה </a></div>
+                          <div><a href="https://github.com/piskarovhaim/VGareus" target="_blank">מעבר לגיט הקבוצה</a></div>
+                      </div>
+                  </Col>
+                  <Col>
+                      <div className="Card">
+                          <div> קבוצה 6 </div>
+                          <div> חברי הקבוצה </div>
+                          <div> ישראל בן מנחם </div>
+                          <div> יורי קובלוב </div>
+                          <div> יאיר ויגודה </div>
+                          <div> נעמי מטרי </div>
+                          <div> לירן אלבוחר </div>
+                          <div> תומר חליבה </div>
+                          <div> דוד בוגומולוב </div>
+                          <div><a href="/">מעבר לפרוייקט הקבוצה</a></div>
+                          <div><a href="https://github.com/KovalyovYura/booksLIb.git" target="_blank">מעבר לגיט הקבוצה</a></div>
+                      </div>
+                  </Col>
+              </Row>
+          </Container>
+
       </header>
     </div>
   );
