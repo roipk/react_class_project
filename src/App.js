6e2cd82c771e722dc7ce28daa22fa8d6ba67273d
Roi Madmon <roipk123@gmail.com>
2020-08-10 17:21:13 +0300

    fix firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="app bg-dark">
           <div className="container pt-5 pb-5">
               <div className="row text-center">
-                    <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                  <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                         <h2 className="h2"> קבוצה 1 </h2>
                         <div> חברי הקבוצה </div>
                         <div> אמג'ד בשיתי </div>
@@ -15,10 +15,10 @@ function App() {
                         <div> רועי סולימני </div>
                         <div> תהילה כהן </div>
                         <div> לואיי ג'אבר </div>
-                        <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                        <div><a href="https://movie-list-88522.web.app/"> מעבר לפרוייקט הקבוצה </a></div>
                         <div><a href="https://github.com/bashbash96/movies-list" target="_blank">מעבר לגיט הקבוצה</a></div>
                     </div>
-                    <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                  <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                         <h2 className="h2"> קבוצה 2 </h2>
                         <div> חברי הקבוצה </div>
                         <div> הדר דבאח </div>
@@ -27,10 +27,10 @@ function App() {
                         <div> רחלי וורכנזון </div>
                         <div> מרים (שבתאי) הרו </div>
                         <div> אודיה טהרני </div>
-                        <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                        <div><a href="https://react-project-vehicles.web.app/"> מעבר לפרוייקט הקבוצה </a></div>
                         <div><a href="https://github.com/hadardabah/react" target="_blank">מעבר לגיט הקבוצה</a></div>
                     </div>
-                    <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                         <h2 className="h2"> קבוצה 3 </h2>
                         <div> חברי הקבוצה </div>
                         <div> נועם לסרי </div>
@@ -39,10 +39,10 @@ function App() {
                         <div> תומר מעלומי </div>
                         <div> שמואל מרקובסקי </div>
                         <div> סמואל נקש </div>
-                        <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                        <div><a href="https://react-final-d6e46.web.app"> מעבר לפרוייקט הקבוצה </a></div>
                         <div><a href="https://github.com/noamlasry/React-Final-Project.git" target="_blank">מעבר לגיט הקבוצה</a></div>
                     </div>
-                        <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                  <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <h2 className="h2"> קבוצה 4 </h2>
                             <div> חברי הקבוצה </div>
                             <div> אנס אבו רמלה  </div>
@@ -50,10 +50,10 @@ function App() {
                             <div> מחמוד קוואסמי </div>
                             <div> אחמד שרבאתי </div>
                             <div> דוחא כסואני </div>
-                            <div><a href="/"> מעבר לפרוייקט הקבוצה </a></div>
+                            <div><a href="https://react-final-project-bd305.web.app"> מעבר לפרוייקט הקבוצה </a></div>
                             <div><a href="https://github.com/anaSameer/ReactFinalProject" target="_blank">מעבר לגיט הקבוצה</a></div>
                         </div>
-                        <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                  <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <h2 className="h2"> קבוצה 5 </h2>
                             <div> חברי הקבוצה </div>
                             <div> לידור זקן </div>
@@ -66,7 +66,7 @@ function App() {
                             <div><a href="https://vgareus-1f6f2.web.app/"> מעבר לפרוייקט הקבוצה </a></div>
                             <div><a href="https://github.com/piskarovhaim/VGareus" target="_blank">מעבר לגיט הקבוצה</a></div>
                         </div>
-                        <div className="Card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                  <div className="Card col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <h2 className="h2"> קבוצה 6 </h2>
                             <div> חברי הקבוצה </div>
                             <div> ישראל בן מנחם </div>
@@ -76,7 +76,7 @@ function App() {
                             <div> לירן אלבוחר </div>
                             <div> תומר חליבה </div>
                             <div> דוד בוגומולוב </div>
-                            <div><a href="/">מעבר לפרוייקט הקבוצה</a></div>
+                            <div><a href="https://itbooks-92386.web.app">מעבר לפרוייקט הקבוצה</a></div>
                             <div><a href="https://github.com/KovalyovYura/booksLIb.git" target="_blank">מעבר לגיט הקבוצה</a></div>
                         </div>
                     </div>
